d72fcd66a3d08b3816517a907ea5ce7fc74d349e
wcastand@gmail.com <wcastand@gmail.com>
2015-04-21 02:36:56 +0200

    Please enter the commit message for your changes. Lines starting

--- js/Preloader.js
+++ js/Preloader.js
@@ -13,31 +13,31 @@ Blblbl.Preloader.prototype = {
     this.load.image(      'title',        './assets/title.png');
     this.load.image(      'ecranScore',   './assets/EcranScore.jpg');
 
-    this.load.image(      'moutain',    '/assets/moutain.jpg');
-    this.load.image(      'top',        '/assets/top.png');
-    this.load.image(      'mur',        '/assets/mur.png');
-    this.load.image(      'bot',        '/assets/bot.png');
+    this.load.image(      'moutain',    './assets/moutain.jpg');
+    this.load.image(      'top',        './assets/top.png');
+    this.load.image(      'mur',        './assets/mur.png');
+    this.load.image(      'bot',        './assets/bot.png');
 
-    this.load.spritesheet('bubble1',     '/assets/bubble1.png',  160, 105, 3);
+    this.load.spritesheet('bubble1',     './assets/bubble1.png',  160, 105, 3);
 
-    this.load.image(      'ball',       '/assets/boule.png');
-    this.load.image(      'noammo',     '/assets/noammo.png');
-    this.load.spritesheet('explosion',  '/assets/shot.png',     80, 80, 2);
-    this.load.spritesheet('scoring',    '/assets/scoring.png',  80, 80, 5);
-    this.load.spritesheet('cailloux',   '/assets/cailloux.png', 36, 66, 8);
-    this.load.spritesheet('bullet',     '/assets/balle.png',    160, 20, 5);
-    this.load.spritesheet('player',     '/assets/player.png',   196, 82, 35);
-    this.load.spritesheet('people1',    '/assets/people1.png',  196, 82, 35);
-    this.load.spritesheet('people2',    '/assets/people2.png',  196, 82, 35);
-    this.load.spritesheet('people3',    '/assets/people3.png',  196, 82, 35);
-    this.load.spritesheet('people4',    '/assets/people4.png',  196, 82, 35);
+    this.load.image(      'ball',       './assets/boule.png');
+    this.load.image(      'noammo',     './assets/noammo.png');
+    this.load.spritesheet('explosion',  './assets/shot.png',     80, 80, 2);
+    this.load.spritesheet('scoring',    './assets/scoring.png',  80, 80, 5);
+    this.load.spritesheet('cailloux',   './assets/cailloux.png', 36, 66, 8);
+    this.load.spritesheet('bullet',     './assets/balle.png',    160, 20, 5);
+    this.load.spritesheet('player',     './assets/player.png',   196, 82, 35);
+    this.load.spritesheet('people1',    './assets/people1.png',  196, 82, 35);
+    this.load.spritesheet('people2',    './assets/people2.png',  196, 82, 35);
+    this.load.spritesheet('people3',    './assets/people3.png',  196, 82, 35);
+    this.load.spritesheet('people4',    './assets/people4.png',  196, 82, 35);
 
-    this.load.audio('shootSound',       'sound/shoot.mp3');
-    this.load.audio('mainSound',        'sound/Main.mp3');
-    this.load.audio('toucheySound',     'sound/touchey.mp3');
-    this.load.audio('loopSound',        'sound/Mainboucle.mp3');
-    this.load.audio('splashSound',      'sound/splash.mp3');
-    this.load.audio('gameOverSound',    'sound/gameover.mp3');
+    this.load.audio('shootSound',       './sound/shoot.mp3');
+    this.load.audio('mainSound',        './sound/Main.mp3');
+    this.load.audio('toucheySound',     './sound/touchey.mp3');
+    this.load.audio('loopSound',        './sound/Mainboucle.mp3');
+    this.load.audio('splashSound',      './sound/splash.mp3');
+    this.load.audio('gameOverSound',    './sound/gameover.mp3');
   },
   create: function () {
     this.state.start('Game');
